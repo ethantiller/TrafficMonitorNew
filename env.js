cc63f9dda7a220e46d8e58b2ffd02e0422d5517f
Ethan Tiller <tilleres@mail.uc.edu>
2025-03-04 21:54:42 -0500

    ragh

--- env.js
+++
@@ -1,8 +0,0 @@
-window.__ENV = {
-  // Auth0 credentials
-  AUTH0_DOMAIN: "dev-h2kl7dklje1ovh16.us.auth0.com", // String
-  AUTH0_CLIENT_ID: "IZD9ObtBRNHd1IXlJY3avZqTkpB3Fs4r", // String
-  
-  // Google Maps API Key
-  GOOGLE_MAPS_API_KEY: "AIzaSyAZmKNiPGUqEQxs3uovyiHUbTjzEKfRjKA" // String
-};
