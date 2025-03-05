b55f1befed37283fa8ed84e66276868b695becbc
Ethan Tiller <tilleres@mail.uc.edu>
2025-03-04 21:48:53 -0500

    Cry

--- env.js
+++ env.js
@@ -1,9 +1,8 @@
-// This file exposes environment variables to the browser
 window.__ENV = {
   // Auth0 credentials
-  AUTH0_DOMAIN: "AUTH0_DOMAIN",
-  AUTH0_CLIENT_ID: "AUTH0_CLIENT_ID",
+  AUTH0_DOMAIN: "dev-h2kl7dklje1ovh16.us.auth0.com", // String
+  AUTH0_CLIENT_ID: "IZD9ObtBRNHd1IXlJY3avZqTkpB3Fs4r", // String
   
   // Google Maps API Key
-  GOOGLE_MAPS_API_KEY: "GOOGLE_MAPS_API_KEY"
+  GOOGLE_MAPS_API_KEY: "AIzaSyAZmKNiPGUqEQxs3uovyiHUbTjzEKfRjKA" // String
 };
