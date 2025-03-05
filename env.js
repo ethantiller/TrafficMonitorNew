6b3e3627304e2359b1296c82ef73c11a64bc0670
Ethan Tiller <tilleres@mail.uc.edu>
2025-03-04 21:36:52 -0500

    wieuriwehgwpiuerg

--- env.js
+++ env.js
@@ -1,9 +1,9 @@
 // This file exposes environment variables to the browser
 window.__ENV = {
   // Auth0 credentials
-  AUTH0_DOMAIN: AUTH0_DOMAIN,
-  AUTH0_CLIENT_ID: AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: "AUTH0_DOMAIN",
+  AUTH0_CLIENT_ID: "AUTH0_CLIENT_ID",
   
   // Google Maps API Key
-  GOOGLE_MAPS_API_KEY: GOOGLE_MAPS_API_KEY
+  GOOGLE_MAPS_API_KEY: "GOOGLE_MAPS_API_KEY"
 };
